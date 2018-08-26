9eb363efeb8cf26d44333ae9f383f22e9fcfff4e
Alexa Schaeffer <alps231@gmail.com>
2018-08-26 20:37:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-const recipe = {
-  flour = "1 cup",
-  corn = "1 husk",
-  salt = "2 tablespoons",
-  sugar = "half a cup",
+var recipe = {
+  flour: "1 cup",
+  corn: "1 husk",
+  saltL: "2 tablespoons",
+  sugar: "half a cup",
 }
+
+function updateObjectWithKeyAndValue(recipe, ingredient, quantity){
+  recipe = [ingredient]: quantity
+  return Object.assign({}, ingredient, quantity)
+}
+
+const recipe = {vanilla: "1 teaspoon"}
